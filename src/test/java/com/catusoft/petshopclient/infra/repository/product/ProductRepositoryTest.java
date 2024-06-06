1b1570af097cbc7ff743bb9500b87d6f4a6c66e0
Mateus Catureba <caturebadev@gmail.com>
2024-06-06 20:40:53 -0300

    tests(): adicionando mais testes de integração

--- src/test/java/com/catusoft/petshopclient/infra/repository/product/ProductRepositoryTest.java
+++ src/test/java/com/catusoft/petshopclient/infra/repository/product/ProductRepositoryTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class ProductRepositoryTest {
